ffadd3988f5cfc2cbbd250c9f8e82635b8c3995a
SonicGD <sonicgd@gmail.com>
2014-09-18 12:27:02 +0600

    fix for +

--- CacheActiveQuery.php
+++ CacheActiveQuery.php
@@ -16,95 +16,6 @@ class CacheActiveQuery extends ActiveQuery
     private $dropConditions = [];
     private $noCache = false;
 
-    private function getParsedWhere($sql = null)
-    {
-        $where = [];
-        if (!$sql) {
-            $sql = $this->createCommand()->rawSql;
-        }
-        $parser = new Parser();
-        $parsed = $parser->parse($sql);
-        if (isset($parsed['WHERE']) && count($parsed['WHERE']) > 0) {
-            //var_dump($parsed['WHERE']);
-            $this->parseWhere($parsed['WHERE'], $where);
-        }
-        return $where;
-    }
-
-    private function parseWhere($parsedWhere, &$where)
-    {
-        for ($i = 0; $i < count($parsedWhere);) {
-            $element = $parsedWhere[$i];
-            switch ($element['expr_type']) {
-                case 'colref':
-                    $operator = $parsedWhere[$i + 1]['base_expr'];
-                    $value = $this->getWhereValue($parsedWhere, $operator, $i);
-                    $where[] = [
-                        'col'      => trim($element['base_expr'], '`'),
-                        'operator' => $operator,
-                        'value'    => $value,
-                    ];
-                    $i += 3;
-                    break;
-                case 'operator':
-                    $i++;
-                    break;
-                case 'bracket_expression':
-                    $this->parseWhere($element['sub_tree'], $where);
-                    $i++;
-                    break;
-                default:
-                    $i++;
-                    break;
-            }
-        }
-        return $where;
-    }
-
-    /**
-     * @param $parsedWhere
-     * @param $operator
-     * @param $i
-     * @return array|string
-     */
-    private function getWhereValue($parsedWhere, &$operator, $i)
-    {
-        $value = null;
-        $valueType = $parsedWhere[$i + 2]['expr_type'];
-        switch ($valueType) {
-            case 'const':
-                $value = trim($parsedWhere[$i + 2]['base_expr'], "'");
-                break;
-            case 'in-list':
-                $value = [];
-                foreach ($parsedWhere[$i + 2]['sub_tree'] as $subElement) {
-                    if ($subElement['expr_type'] == 'const') {
-                        $value[] = $subElement['base_expr'];
-                    } else {
-                        //TODO: smthng
-                    }
-                }
-                break;
-            case 'operator':
-                switch ($parsedWhere[$i + 2]['base_expr']) {
-                    case 'IN':
-                        if ($operator == 'NOT') {
-                            $tmp = 'IN';
-                            $value = $this->getWhereValue($parsedWhere, $tmp, $i + 1);
-                            $operator = "NOT IN";
-                        }
-                        break;
-                    default:
-                        die($parsedWhere[$i + 2]['base_expr']);
-                }
-                break;
-            default:
-                die($valueType);
-                break;
-        }
-        return $value;
-    }
-
     /**
      * @inheritdoc
      */
@@ -167,6 +78,225 @@ class CacheActiveQuery extends ActiveQuery
     }
 
     /**
+     * @param string  $sql
+     *
+     * @param         $mode
+     *
+     * @return string
+     */
+    private function generateCacheKey($sql, $mode)
+    {
+        $key = $mode;
+        $key .= strtolower($this->modelClass);
+        $key .= $sql;
+        if (count($this->where) == 0 && count($this->dropConditions) == 0) {
+            $this->dropCacheOnCreate();
+        }
+        //pagination
+        if ($this->limit > 0) {
+            $key .= "limit" . $this->limit;
+        }
+        if ($this->offset > 0) {
+            $key .= "offset" . $this->offset;
+        }
+        \Yii::info(
+            "Generate cache key for " . $key . ':  . md5($key)',
+            'cache'
+        );
+        return md5($key);
+    }
+
+    /**
+     * @param string|null  $param
+     * @param string|array $value
+     *
+     * @return self
+     */
+    public function dropCacheOnCreate($param = null, $value = null)
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        foreach ($value as $val) {
+            $event = [
+                'type'  => 'create',
+                'param' => $param,
+                'value' => $val
+            ];
+            $this->dropConditions[] = $event;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param                $key
+     * @param ActiveRecord[] $models
+     *
+     * @return bool
+     */
+    private function insertInCacheAll($key, $models)
+    {
+        /** @var $class ActiveRecord */
+        $class = $this->modelClass;
+        $indexes = [
+            $class::tableName() => [
+            ]
+        ];
+        if ($models) {
+            $toCache = $models;
+            foreach ($toCache as $index => $model) {
+                $mToCache = clone $model;
+                $mToCache->fromCache = true;
+                $toCache[$index] = $mToCache;
+                $pks = $mToCache->getPrimaryKey(true);
+                $indexes[$class::tableName()][] = reset($pks);
+            }
+        } else {
+            $toCache = ['null'];
+            $indexes[$class::tableName()][] = null;
+            $this->generateDropConditionsForEmptyResult();
+        }
+
+        ActiveQueryCacheHelper::insertInCache($key, $toCache, $indexes, $this->dropConditions);
+
+        return true;
+    }
+
+    /**
+     */
+    private function generateDropConditionsForEmptyResult()
+    {
+
+        if (count($this->where) == 0) {
+            $this->dropCacheOnCreate();
+        } else {
+            $where = $this->getParsedWhere();
+            foreach ($where as $condition) {
+                if (in_array(
+                    $condition['operator'],
+                    [
+                        'NOT IN',
+                        '!=',
+                        '>',
+                        '<',
+                        '>=',
+                        '<=',
+                    ]
+                )) {
+                    continue;
+                }
+                if (!$condition['value']) {
+                    continue;
+                }
+                $this->dropCacheOnCreate($condition['col'], $condition['value']);
+            }
+        }
+    }
+
+    private function getParsedWhere($sql = null)
+    {
+        $where = [];
+        if (!$sql) {
+            $sql = $this->createCommand()->rawSql;
+        }
+        $parser = new Parser();
+        $parsed = $parser->parse($sql);
+        if (isset($parsed['WHERE']) && count($parsed['WHERE']) > 0) {
+            //var_dump($parsed['WHERE']);
+            $this->parseWhere($parsed['WHERE'], $where);
+        }
+        return $where;
+    }
+
+    private function parseWhere($parsedWhere, &$where)
+    {
+        for ($i = 0; $i < count($parsedWhere);) {
+            $element = $parsedWhere[$i];
+            switch ($element['expr_type']) {
+                case 'colref':
+                    $operator = $parsedWhere[$i + 1]['base_expr'];
+                    $value = $this->getWhereValue($parsedWhere, $operator, $i);
+                    $where[] = [
+                        'col'      => trim($element['base_expr'], '`'),
+                        'operator' => $operator,
+                        'value'    => $value,
+                    ];
+                    $i += 3;
+                    break;
+                case 'operator':
+                    $i++;
+                    break;
+                case 'bracket_expression':
+                    $this->parseWhere($element['sub_tree'], $where);
+                    $i++;
+                    break;
+                default:
+                    $i++;
+                    break;
+            }
+        }
+        return $where;
+    }
+
+    /**
+     * @param $parsedWhere
+     * @param $operator
+     * @param $i
+     * @return array|string
+     */
+    private function getWhereValue($parsedWhere, &$operator, &$i)
+    {
+        $value = null;
+        $valueType = $parsedWhere[$i + 2]['expr_type'];
+        switch ($valueType) {
+            case 'const':
+                $value = trim($parsedWhere[$i + 2]['base_expr'], "'");
+                break;
+            case 'in-list':
+                $value = [];
+                foreach ($parsedWhere[$i + 2]['sub_tree'] as $subElement) {
+                    if ($subElement['expr_type'] == 'const') {
+                        $value[] = $subElement['base_expr'];
+                    } else {
+                        //TODO: smthng
+                    }
+                }
+                break;
+            case 'colref':
+                switch ($operator) {
+                    case '+':
+                        $value = trim($parsedWhere[$i + 3]['base_expr'], "'");
+                        $i++;
+                        break;
+                    default:
+                        die($parsedWhere[$i + 2]);
+                }
+                break;
+            case 'operator':
+                switch ($parsedWhere[$i + 2]['base_expr']) {
+                    case 'IN':
+                        if ($operator == 'NOT') {
+                            $tmp = 'IN';
+                            $value = $this->getWhereValue($parsedWhere, $tmp, $i + 1);
+                            $operator = "NOT IN";
+                        }
+                        break;
+                    default:
+                        die($parsedWhere[$i + 2]);
+                }
+                break;
+            default:
+                var_dump($i);
+                var_dump($parsedWhere);
+                die($valueType);
+                break;
+        }
+        return $value;
+    }
+
+    /**
      * @inheritdoc
      */
     public function one($db = null)
@@ -224,35 +354,6 @@ class CacheActiveQuery extends ActiveQuery
     }
 
     /**
-     * @param string  $sql
-     *
-     * @param         $mode
-     *
-     * @return string
-     */
-    private function generateCacheKey($sql, $mode)
-    {
-        $key = $mode;
-        $key .= strtolower($this->modelClass);
-        $key .= $sql;
-        if (count($this->where) == 0 && count($this->dropConditions) == 0) {
-            $this->dropCacheOnCreate();
-        }
-        //pagination
-        if ($this->limit > 0) {
-            $key .= "limit" . $this->limit;
-        }
-        if ($this->offset > 0) {
-            $key .= "offset" . $this->offset;
-        }
-        \Yii::info(
-            "Generate cache key for " . $key . ':  . md5($key)',
-            'cache'
-        );
-        return md5($key);
-    }
-
-    /**
      * @param              $key
      * @param ActiveRecord $model
      *
@@ -280,64 +381,6 @@ class CacheActiveQuery extends ActiveQuery
         ActiveQueryCacheHelper::insertInCache($key, $toCache, $indexes, $this->dropConditions);
 
         return true;
-    }
-
-    /**
-     * @param                $key
-     * @param ActiveRecord[] $models
-     *
-     * @return bool
-     */
-    private function insertInCacheAll($key, $models)
-    {
-        /** @var $class ActiveRecord */
-        $class = $this->modelClass;
-        $indexes = [
-            $class::tableName() => [
-            ]
-        ];
-        if ($models) {
-            $toCache = $models;
-            foreach ($toCache as $index => $model) {
-                $mToCache = clone $model;
-                $mToCache->fromCache = true;
-                $toCache[$index] = $mToCache;
-                $pks = $mToCache->getPrimaryKey(true);
-                $indexes[$class::tableName()][] = reset($pks);
-            }
-        } else {
-            $toCache = ['null'];
-            $indexes[$class::tableName()][] = null;
-            $this->generateDropConditionsForEmptyResult();
-        }
-
-        ActiveQueryCacheHelper::insertInCache($key, $toCache, $indexes, $this->dropConditions);
-
-        return true;
-    }
-
-    /**
-     * @param string|null  $param
-     * @param string|array $value
-     *
-     * @return self
-     */
-    public function dropCacheOnCreate($param = null, $value = null)
-    {
-        if (!is_array($value)) {
-            $value = [$value];
-        }
-
-        foreach ($value as $val) {
-            $event = [
-                'type'  => 'create',
-                'param' => $param,
-                'value' => $val
-            ];
-            $this->dropConditions[] = $event;
-        }
-
-        return $this;
     }
 
     /**
@@ -389,37 +432,6 @@ class CacheActiveQuery extends ActiveQuery
         $params = [];
         $class = $this->modelClass;
         return $class::deleteAll($this->where, $params);
-    }
-
-    /**
-     */
-    private function generateDropConditionsForEmptyResult()
-    {
-
-        if (count($this->where) == 0) {
-            $this->dropCacheOnCreate();
-        } else {
-            $where = $this->getParsedWhere();
-            foreach ($where as $condition) {
-                if (in_array(
-                    $condition['operator'],
-                    [
-                        'NOT IN',
-                        '!=',
-                        '>',
-                        '<',
-                        '>=',
-                        '<=',
-                    ]
-                )) {
-                    continue;
-                }
-                if (!$condition['value']) {
-                    continue;
-                }
-                $this->dropCacheOnCreate($condition['col'], $condition['value']);
-            }
-        }
     }
 
 
