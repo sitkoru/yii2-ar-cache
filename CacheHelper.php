4d8603d6420038fe4b95f8fad826c8c0700bad41
SonicGD <sonicgd@gmail.com>
2014-12-22 17:13:43 +0500

    allow override redis

--- CacheHelper.php
+++ CacheHelper.php
@@ -21,41 +21,41 @@ class CacheHelper
 
     public static function addToSet($setKey, $member)
     {
-        return self::getRedis()->executeCommand("SADD", [$setKey, $member]);
+        return static::getRedis()->executeCommand("SADD", [$setKey, $member]);
     }
 
     public static function getSetMembers($setKey)
     {
-        return self::getRedis()->executeCommand("SMEMBERS", [$setKey]);
+        return static::getRedis()->executeCommand("SMEMBERS", [$setKey]);
     }
 
     public static function removeFromSet($setKey, $member)
     {
-        return self::getRedis()->executeCommand("SREM", [$setKey, $member]);
+        return static::getRedis()->executeCommand("SREM", [$setKey, $member]);
     }
 
     public static function addToList($listKey, $member)
     {
-        return self::getRedis()->executeCommand("LPUSH", [$listKey, $member]);
+        return static::getRedis()->executeCommand("LPUSH", [$listKey, $member]);
     }
 
     public static function getListMembers($listKey, $start = 0, $length = -1)
     {
-        return self::getRedis()->executeCommand("LRANGE", [$listKey, $start, $length]);
+        return static::getRedis()->executeCommand("LRANGE", [$listKey, $start, $length]);
     }
 
     public static function getListLength($listKey)
     {
-        return self::getRedis()->executeCommand("LLEN", [$listKey]);
+        return static::getRedis()->executeCommand("LLEN", [$listKey]);
     }
 
     public static function deleteList($listKey)
     {
-        return self::getRedis()->executeCommand("DEL", [$listKey]);
+        return static::getRedis()->executeCommand("DEL", [$listKey]);
     }
 
     public static function increment($key)
     {
-        return self::getRedis()->executeCommand("INCR", [$key]);
+        return static::getRedis()->executeCommand("INCR", [$key]);
     }
 }
